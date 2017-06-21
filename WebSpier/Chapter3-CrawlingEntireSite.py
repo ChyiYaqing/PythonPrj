fe1235cfb4b836802f4ba6f37d3e53f44e1d1da9
ChyiYaqing <chyiyaqing@gmail.com>
2017-06-22 00:00:14 +0800

    Crawling an Entire Site

---
+++ WebSpier/Chapter3-CrawlingEntireSite.py
@@ -0,0 +1,65 @@
+#!/usr//bin/env python3
+#-*- coding: utf-8 -*-
+#===========================================
+#
+#           FILE: Chapter3-CrawlingEntireSite.py
+#
+#           USAGE:
+#
+#   DESCRIPTION:
+#
+#       OPTIONS: ----
+#  REQUIREMENTS: ----
+#          BUGS: ----
+#         NOTES: ----
+#        AUTHOR: Chyi Yaqing
+#  ORGANIZATION:
+#       VERSION: 0.1.0
+#       CREATED: 04/24/2017
+#      REVISION: ----
+#      Copyright © 2017 Chyi Yaqing
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software FOundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAB PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+#===================================================================
+"""
+By modifying our basic crawling code, we can create a combination crawler/data-gathering(or, at least, data printing) program
+"""
+
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+import re
+
+pages = set()
+
+def getLinks(pageUrl):
+    global pages
+    html = urlopen("http://en.wikipedia.org"+pageUrl)
+    bsObj = BeautifulSoup(html, "html.parser")
+
+    try:
+        print(bsObj.h1.get_text())
+        print(bsObj.find(id="mw-content-text").findAll("p")[0])
+        print(bsObj.find(id="ca-edit").find("span").find("a").attrs['href'])
+    except AttributeError:
+        print("This page is missing something! No worries though!")
+
+    for link in bsObj.findAll("a", href=re.compile("^(/wiki/)")):
+        if 'href' in link.attrs:
+            if link.attrs['href'] not in pages:
+                # we have encountered a new page
+                newPage = link.attrs['href']
+                print("---------------\n"+newPage)
+                pages.add(newPage)
+                getLinks(newPage)
+
+getLinks("")
